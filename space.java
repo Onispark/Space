bfddefd676967c811f75c8d7f8bb0dde0ce938e0
Onyi Wogu <o.wogu@lancaster.ac.uk>
2020-10-08 22:45:09 +0100

    Rotation of the earth added

--- space.java
+++ space.java
@@ -5,13 +5,14 @@ public class space
         int program_running = 1;
 
         SolarSystem a = new SolarSystem(800, 500);
+        int i = 0;
         while (program_running == 1)
         {
-            a.drawSolarObject(0.0, 0.0, 100.0, "YELLOW");
 
-            a.drawSolarObjectAbout(10.0, 20.0, 20.0, "BLUE", 0, 10);
-
-            a.finishedDrawing();
+                a.drawSolarObject(0.0, 0.0, 100.0, "YELLOW");
+                a.drawSolarObjectAbout(150.0, i, 20.0, "BLUE", 0.0, 130.0);
+                a.finishedDrawing();
+                i++;
         }
     
     }    
