dfdea33c9bd5eeaf7d6c96793338e66739b81bf5
Onyi Wogu <o.wogu@lancaster.ac.uk>
2020-10-22 19:40:53 +0100

    Stars added, unused classes removed, comments added, code formatted

--- space.java
+++ space.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-import java.lang.Math;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class space
@@ -12,31 +10,43 @@ public class space
         int i = 0; // variable used to dictate the speed of the individual planets
         int j = 1;
 
+        //initialising arrays for star values
+        double[] starDistance = new double[401];
+        double[] starAngle = new double[401];
+        double[] starDiameter = new double[401];
+
+        //for loop to add the stars to the solar system.
+        for (int y = 0; y<=400; y++)
+        {
+            starDistance[y] = ThreadLocalRandom.current().nextDouble(0, 1000);
+            starAngle[y] = ThreadLocalRandom.current().nextDouble(0, 1000);
+            starDiameter[y] = ThreadLocalRandom.current().nextDouble(1, 4);
+        }
+
+        //initialising arrays for asteroid values
         double[] angle = new double[306];
         int[] diameter = new int[306];
         int[] distance = new int[306];
         int[] space_apart = new int[306];
         double[] shape = new double[306];
 
-
+        //For loop to generate random numbers within a range to ensure each asteroid is unique(adds realism)
         
         for (int k = 0; k <= 305; k++){
-            angle[k] = ThreadLocalRandom.current().nextDouble(0.50, 0.85);
-            diameter[k] = ThreadLocalRandom.current().nextInt(1, 3 + 1);
-            distance[k]=ThreadLocalRandom.current().nextInt(100, 124 + 1);
-            space_apart[k] = ThreadLocalRandom.current().nextInt(k*10, k*12 + 1);
-            shape[k] = ThreadLocalRandom.current().nextDouble(1, 3.5);
+            angle[k] = ThreadLocalRandom.current().nextDouble(0.50, 0.85); //unique speed
+            diameter[k] = ThreadLocalRandom.current().nextInt(1, 3 + 1); //unique size
+            distance[k]=ThreadLocalRandom.current().nextInt(100, 124 + 1); //unqiue distance
+            space_apart[k] = ThreadLocalRandom.current().nextInt(k*10, k*12 + 1);//unique starting point
+            shape[k] = ThreadLocalRandom.current().nextDouble(1, 3.5); //unique orbitory shape
         }
-
-
-
-    
 
 
         while (j==1) //creates an infinate loop so the planets are animated
         {
-
-
+            for (int y =0; y <= 400; y++)
+            {
+                a.drawSolarObject(starDistance[y], starAngle[y], starDiameter[y],"WHITE");
+            }
 
             //adding planets to the solar system
             a.drawSolarObject(0.0, 0.0, 35.0, "YELLOW");
@@ -47,18 +57,17 @@ public class space
             a.drawSolarObjectAbout(154.0, ((i*0.868)), 33.0, "#5c272b", 1.0, 0.0); //JUPITER
             a.drawSolarObjectAbout(212.0, ((i*0.646)), 27.0, "#656e48", 2.0, 0.0);//SATURN
             a.drawSolarObjectAbout(327.0, ((i*0.456)), 12.0, "#90adad", 3.0, 0.0);//URANUS
-            a.drawSolarObjectAbout(400.0, ((i*0.364)), 8.0, "#3f485e", 2.0, 0.0); //NEPTUNE
-            
-            for (int x = 0; x <= 305; x++){
-            a.drawSolarObjectAbout(distance[x], (space_apart[x]+(i*angle[x])), diameter[x], "#8c8d8f", shape[x], 0.0);
-            }
-
-    
-               
-    
+            a.drawSolarObjectAbout(400.0, ((i*0.364)), 8.0, "#3f485e", 2.0, 0.0); //NEPTUNE               
 
             //adding the moons
+
+            //to get the correct orbit for the moon the distance and angle of the planet the moon orbited was used
+            // as the centreofrotationdistance and centreofrotationangle
+
+            //EARTH MOON
             a.drawSolarObjectAbout(12.0, (i*4.76), 2.0, "GREY", (68.0),(i*2.0)); //EARTH MOON
+
+            //MARS MOONS
             a.drawSolarObjectAbout(10.0, (180+(i*4.86)), 2.0, "GREY", 85.0, ((i*1.604))); //MARS MOON PHOBOSS
             a.drawSolarObjectAbout(13.0, (i*4.45), 2.0, "GREY", 85.0, ((i*1.604))); //MARS MOON DEIMOS
 
@@ -70,11 +79,16 @@ public class space
             //SATURN MOONS
             a.drawSolarObjectAbout(31.5, (i*4.50), 4.0, "GREY", 212.0, ((i*0.646))); //TITAN
 
+            //next add asteroid belt
+            for (int x = 0; x <= 305; x++) //iterate through loop to add asteroid belt.
+            {
+                a.drawSolarObjectAbout(distance[x], (space_apart[x]+(i*angle[x])), diameter[x], "#8c8d8f", shape[x], 0.0);
+            }
+
             a.finishedDrawing();
 
             i++;
 
-            //next add asteroid belt
         }   
     }       
 }
